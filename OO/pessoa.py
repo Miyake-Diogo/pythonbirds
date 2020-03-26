744896869278e6c8e228052bcb06ae4d60b186b9
Diogo <akio.miyake@live.com>
2020-03-26 09:53:43 -0300

    criado metodo init

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,8 +1,17 @@
 class Pessoa:
+    def __init__(self, nome = None, idade=None):
+        self.idade = idade
+        self.nome = nome # lado direito atributo, esquerdo recebe o parametro
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Jamile')
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Diogo'
+    print(p.nome)
+
+
