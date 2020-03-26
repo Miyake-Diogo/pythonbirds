38d1e27912dcc191b5046871ac0ad262822aa0c3
Diogo <akio.miyake@live.com>
2020-03-26 10:52:51 -0300

    criado atributos complexos

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,17 +1,23 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=None):
+    def __init__(self, *filhos,nome = None, idade=28):
         self.idade = idade
         self.nome = nome # lado direito atributo, esquerdo recebe o parametro
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Jamile')
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Diogo'
-    print(p.nome)
+    olivia = Pessoa(nome='Olivia')
+    lara = Pessoa(nome='Lara')
+    jamile = Pessoa(olivia, nome='Jamile')
+    diogo = Pessoa(nome='Diogo', idade=30)
+    print(Pessoa.cumprimentar(diogo))
+    print(id(jamile))
+    print(jamile.nome)
+    print(jamile.idade)
+    for filho in jamile.filhos:
+        print(filho.nome)
+    print(diogo.idade)
 
 
